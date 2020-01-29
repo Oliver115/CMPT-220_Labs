61b42a0804e76f774c3e211fc0d447b456d53d99
Oliver <oliver.wilson1@marist.edu>
2020-01-28 21:33:49 -0500

    Initial Lab01 commit

---
+++ src/Lab01/HelloWorld.java
@@ -0,0 +1,14 @@
+package Lab01;
+
+public class HelloWorld {
+
+    /**
+     * Obligatory starter code.
+     * @param args command line arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
